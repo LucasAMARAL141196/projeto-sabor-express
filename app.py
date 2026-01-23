2af63bb7f593784fffb4d78c895b9ade98b5c235
LucasAMARAL141196 <lamarallucas603@gmail.com>
2026-01-23 00:42:49 -0300

    Resolvendo conflito de "duplicação de código"

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import os # importando o módulo os para limpar a tela
 
 def exibir_nome_do_programa(): # função para exibir o nome do programa
@@ -40,47 +40,4 @@ def main(): # função principal do app
     escolher_opcao()
 
 if __name__ == '__main__': # função para rodar o app
-=======
-import os # importando o módulo os para limpar a tela
-
-def exibir_nome_do_programa(): # função para exibir o nome do programa
-    print("""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
-""")
-
-def exibir_opcoes(): # função para exibir as opcoes do menu
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
-
-def finalizar_app(): # função para finalizar o app
-    os.system('cls')
-    print('Finalizando o app')
-
-def escolher_opcao(): # função para escolher a opção do menu
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
-
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2: 
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3: 
-        print('Ativar restaurante')
-    else: 
-        finalizar_app()
-
-def main(): # função principal do app
-    exibir_nome_do_programa()
-    exibir_opcoes()
-    escolher_opcao()
-
-if __name__ == '__main__': # função para rodar o app
->>>>>>> 15401c1b03615f9f0bccf627a5e934aabc32b06a
     main()
